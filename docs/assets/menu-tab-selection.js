3207c5b7f3c9c3ccf030e97d3a06611424610baa
Junghwan Park <reserve.dev@gmail.com>
2021-05-17 00:49:39 +0900

    update URL

--- docs/assets/menu-tab-selection.js
+++
@@ -1,7 +0,0 @@
-var menuTabScript = $("script[src*=menu-tab-selection]");
-var pageId = menuTabScript.attr("page-id");
-
-$(".main-content-menu .nav-item").removeClass("nav-select");
-$(".main-content-menu .nav-link[data-id='" + pageId + "']")
-  .parent(".nav-item")
-  .addClass("nav-select");
