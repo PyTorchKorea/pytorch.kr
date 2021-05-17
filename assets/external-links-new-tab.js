b89e4fbf88f5426e22d17bee989386b2aa052ece
Junghwan Park <reserve.dev@gmail.com>
2021-05-17 23:31:33 +0900

    fix links' target property by url

--- assets/external-links-new-tab.js
+++ assets/external-links-new-tab.js
@@ -1,7 +1,10 @@
 var links = document.links;
 
 for (var i = 0; i < links.length; i++) {
-  if (links[i].hostname != window.location.hostname) {
+  if (!links[i].hostname.includes("pytorch.kr")) {
     links[i].target = '_blank';
-  } 
+  }
+  // if (links[i].hostname != window.location.hostname) {
+  //   links[i].target = '_blank';
+  // }
 }
